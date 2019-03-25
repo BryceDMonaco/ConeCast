e9aa6f20194e0316d652e8c4dd133db60e876ad6
Bryce Monaco <brycedmonaco@gmail.com>
2019-03-24 18:12:26 -0700

    Added the ability to specify a starting radius

--- ConeCastExtension.cs
+++ ConeCastExtension.cs
@@ -3,11 +3,44 @@ using UnityEngine;
 
 public static class ConeCastExtension
 {
+    public static RaycastHit[] ConeCastAll(this Physics physics, Vector3 origin, float minRadius, float maxRadius, Vector3 direction, float maxDistance, float coneAngle)
+    {
+        RaycastHit[] coneCastHits = ConeCastAll(physics, origin, maxRadius, direction, maxDistance, coneAngle);
+        RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin, minRadius, direction, maxDistance);  // Can hit objects which were already hit by the ConeCast
+
+        List<RaycastHit> combinedHits = new List<RaycastHit>();
+
+        combinedHits.AddRange(coneCastHits);
+        combinedHits.AddRange(sphereCastHits);
+
+
+        // Remove hits which share the same GameObject TODO Find a more efficient way to get rid of duplicates
+        List<GameObject> individualObjects = new List<GameObject>();
+
+        foreach (RaycastHit hit in combinedHits)
+        {
+            if (!individualObjects.Contains(hit.transform.gameObject))
+            {
+                individualObjects.Add(hit.transform.gameObject);
+
+            } else  // This is a duplicate GameObject
+            {
+                combinedHits.Remove(hit);
+
+            }
+
+        }
+
+        // Return the list of RaycastHits containing no duplicates (no duplucate GameObjects)
+        return combinedHits.ToArray();
+
+    }
+
     public static RaycastHit[] ConeCastAll(this Physics physics, Vector3 origin, float maxRadius, Vector3 direction, float maxDistance, float coneAngle)
     {
-        RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin - new Vector3(0,0,maxRadius), maxRadius, direction, maxDistance);
+        RaycastHit[] sphereCastHits = Physics.SphereCastAll(origin - new Vector3(0, 0, maxRadius), maxRadius, direction, maxDistance);
         List<RaycastHit> coneCastHitList = new List<RaycastHit>();
-        
+
         if (sphereCastHits.Length > 0)
         {
             for (int i = 0; i < sphereCastHits.Length; i++)
